586108b9fda250cef7e43e49742ace0aa2e0871e
pmhieu1309 <pmhieudev1309@gmail.com>
2025-10-15 21:05:23 +0700

    Validated Category.

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::patch('/categories/{id}', [CategoryController::class, 'update']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::apiResource('categories', CategoryController::class);
