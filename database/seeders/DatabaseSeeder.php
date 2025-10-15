0fa538740a94106ea62a1732a0082cd8680a4ddb
buithamky <kyn827765@gmail.com>
2025-10-15 23:54:14 +0700

    CRUD User.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CategorySeeder::class);
+        $this->call(UserSeeder::class);
         // User::factory(10)->create();
 
         // User::factory()->create([
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,24 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// =============== Categories ROUTES ===============
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::patch('/categories/{id}', [CategoryController::class, 'update']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+
+// =============== USER LIST ROUTES ===============
+// Route::get('/users', [UserController::class, 'index']);        
+// Route::post('/users', [UserController::class, 'store']);       
+// Route::get('/users/{id}', [UserList_Controller::class, 'show']);    
+// Route::patch('/users/{id}', [UserList_Controller::class, 'update']); 
+// Route::delete('/users/{id}', [UserList_Controller::class, 'destroy']); 
+Route::apiResource('users', UserController::class);
