19da32a97dd1fad1e7cd947b495579b210c0d966
pmhieu1309 <pmhieudev1309@gmail.com>
2025-10-24 19:50:47 +0700

    Fix crud variation.

--- app/Http/Controllers/VariationController.php
+++ app/Http/Controllers/VariationController.php
@@ -4,93 +4,133 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreVariationRequest;
 use App\Http\Requests\UpdateVariationRequest;
-use App\Models\Variation;
+use App\Services\VariationService;
+use Exception;
 use Illuminate\Http\Request;
 
 class VariationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $variationService;
+
+    public function __construct(VariationService $variationService)
     {
-        $variations = Variation::all();
-        return response()->json(['success' => true, 'variations' => $variations], 200);
+        $this->variationService = $variationService;
     }
 
+    /**
+    * Display a listing of the resource.
+    */
+    public function index()
+    {
+        try {
+            $variations = $this->variationService->getAll();
+
+            return response()->json([
+                'success' => true,
+                'variations' => $variations
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created resource in storage. 
      */
     public function store(StoreVariationRequest $request)
     {
-        $validated = $request->validated();
-        $variation = Variation::create($validated);
+        $data = $request->validated();
+        try {
+            $variation = $this->variationService->saveCategoryData($data);
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Biến thể đã được tạo thành công.',
-            'variation' => $variation
-        ], 201);
+            return response()->json([
+                'success' => true,
+                'message' => 'Biến thể sản phẩm đã được tạo thành công.',
+                'data' => $variation
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Variation $variation)
+    * Display the specified resource.
+    */
+    public function show($id)
     {
-        //
+        try {
+            $variation = $this->variationService->getById($id);
+            return response()->json([
+                'success' => false,
+                'variation' => $variation
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
-     * Update the specified resource in storage.
-     */
+    * Update the specified resource in storage.
+    */
     public function update(UpdateVariationRequest $request, $id)
     {
-        $variation = Variation::findOrFail($id);
+        $data = $request->validated();
 
-        $variation->update($request->validated());
+        try {
+            $variation = $this->variationService->updateCategory($data, $id);
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Cập nhật biến thể thành công.',
-            'data' => $variation
-        ], 200);
+            return response()->json([
+                'success' => true,
+                'message' => 'Biến thể sản phẩm đã được cập nhật thành công.',
+                'data' => $variation
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
-     */
+    * Remove the specified resource from storage.
+    */
     public function destroy($id)
     {
-        $variation = Variation::findOrFail($id);
-        $variation->delete();
+        try {
+            $variation = $this->variationService->deleteById($id);
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Xóa danh biến thể thành công.',
-            'deleted_id' => $id
-        ]);
+            return response()->json([
+                'success' => true,
+                'message' => 'Xóa biến thể sản phẩm thành công.',
+                'variation' => $variation,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 
     /**
-     * Get variations by variation name
-     */
-    public function searchByVariationName(Request $request)
-    {
-        // return "Minh Hieu";
-        $name = $request->query('name');
-
-        if (!$name) {
-            return response()->json(['message' => 'Variation name is required for search.'], 400);
-        }
-
-        $variations = Variation::where('variation_name', 'LIKE', '%' . $name . '%')->get();
-
-        if ($variations->isEmpty()) {
-            return response()->json(['message' => 'No products found matching the name.'], 404);
-        }
-
-        return response()->json($variations);
-    }
+    * Get varations by category
+    */
+    // public function getVariationByCategory(Category $category)
+    // {
+    //     $variations = $category->variations; 
+    //     return response()->json([
+    //         'category' => $category,
+    //         'variations' => $variations,
+    //     ]);
+    // }
 }
