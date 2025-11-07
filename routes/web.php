17f610832388fe928a01f7a3294cd565023a1f21
pmhieu1309 <pmhieudev1309@gmail.com>
2025-11-07 21:48:38 +0700

    Login with Facebook.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\FacebookSocialiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(FacebookSocialiteController::class)->group(function(){
+    Route::get('auth/facebook', 'redirectToFacebook')->name('auth.facebook');
+    Route::get('auth/facebook/callback', 'handleFacebookCallback');
 });
