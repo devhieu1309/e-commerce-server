37551b69c8fb21c1a1854732a28d490a9b1ae659
ThuHien126 <hiennhi305.com>
2025-11-09 16:15:04 +0700

    directions-store-branches ver_1_1

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -17,29 +17,29 @@ class AddressSeeder extends Seeder
         $addresses = [
             [
                 'detailed_address' => '12 Phan Đình Phùng',
-                'provinces_id' => '01',   
-                'wards_id' => '00004',
+                'provinces_id' => '01',   //Hà Nội
+                'wards_id' => '00004', //Phường Ba Đình
             ],
             [
                 'detailed_address' => '23 Lê Lợi',
-                'provinces_id' => '20',   
-                'wards_id' => '06172',   
+                'provinces_id' => '20',   //Lạng Sơn
+                'wards_id' => '06172', //Xã Hoàng Văn Thụ  
             ],
             [
                 'detailed_address' => '56 Nguyễn Văn Cừ',
-                'provinces_id' => '92',      
-                'wards_id' => '31135',       
+                'provinces_id' => '92', //Cần Thơ      
+                'wards_id' => '31135',    //Phường Ninh Kiều   
             ],
             [
                 'detailed_address' => '89 Trần Phú',
-                'provinces_id' => '48',     
-                'wards_id' => '20242',       
+                'provinces_id' => '48', //Đà Nẵng    
+                'wards_id' => '20242', //Phường Hải Châu       
             ],
             
             [
                 'detailed_address' => '77 Nguyễn Tất Thành',
-                'provinces_id' => '42',     
-                'wards_id' => '18100',       
+                'provinces_id' => '42', //Hà Tĩnh     
+                'wards_id' => '18100', //Phường Trần Phú       
             ],
         ];
 
