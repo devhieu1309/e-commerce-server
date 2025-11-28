5091431604b95e6f055a46ef58d684405b6c199f
TrucYang <giangtran260404@gmail.com>
2025-11-29 01:24:47 +0700

    wip: detail order

--- app/Services/ShoppingOrderService.php
+++ app/Services/ShoppingOrderService.php
@@ -39,25 +39,45 @@ class ShoppingOrderService
         if (!$order)
             return null;
 
+        $items = $order->orderLines->map(function ($item) {
+            return [
+                'product_item_id' => $item->product_item_id,
+                'product_name' => $item->productItem->product->product_name ?? 'Không có tên sản phẩm',
+                'sku' => $item->productItem->SKU ?? '',
+                'image' => $item->productItem->image ?? null,
+                'price' => $item->price,
+                'quantity' => $item->quantity,
+                'subtotal' => $item->price * $item->quantity, // Tổng của 1 order line
+            ];
+        });
+
+        //Tổng của tất cả các item order line
+        $subtotalAmount = $items->sum('subtotal');
+
+        //Phí vận chuyển
+        $shippingFee = $order->shippingMethod->shipping_method_price ?? 0;
+
+        //Tổng đơn hàng = subtotal + phí vận chuyển
+        $totalAmount = $subtotalAmount + $shippingFee;
+
         return [
             'shop_order_id' => $order->shop_order_id,
-            'user_name' => $order->user->name ?? 'Không xác định',
+            'user' => [
+                'id' => $order->user->user_id ?? null,
+                'name' => $order->user->name ?? 'Không xác định',
+                'email' => $order->user->email ?? null,
+                'phone' => $order->user->phone ?? null,
+            ],
             'order_date' => $order->order_date,
             'payment_type' => $order->paymentMethod->value ?? '',
             'shipping_method' => $order->shippingMethod->shipping_method_name ?? '',
-            'address' => $order->address->detailed_address ?? '',
-            'order_total' => $order->order_total,
+            'shipping_fee' => $shippingFee,
+            // 'address' => $order->address->detailed_address ?? '',
             'order_status' => $order->orderStatus->status ?? '',
-            'items' => $order->orderLines->map(function ($item) {
-                return [
-                    'product_item_id' => $item->product_item_id,
-                    'product_name' => $item->productItem->product->product_name ?? 'Không có tên sản phẩm',
-                    'image' =>  $item->productItem->image ?? null,
-                    'price' => $item->price,
-                    'quantity' => $item->quantity,
-                    'subtotal' => $item->price * $item->quantity,
-                ];
-            })
+            'items' => $items,
+
+            'subtotal_amount' => $subtotalAmount,
+            'total_amount' => $totalAmount
         ];
 
     }
