5ee0c621e73dcbe7fabbc671df0aa27636771e53
TrucYang <giangtran260404@gmail.com>
2025-12-02 22:37:11 +0700

    feat: update status order

--- app/Models/Order_Status.php
+++ app/Models/Order_Status.php
@@ -9,10 +9,16 @@ class Order_Status extends Model
 {
     use HasFactory;
     protected $table = 'order_status';
+    protected $primaryKey = 'order_status_id';
 
 
     protected $fillable = [
         'status'
 
     ];
+
+    public function shoppingOrders()
+    {
+        return $this->hasMany(ShoppingOrder::class, 'order_status_id');
+    }
 }
--- app/Repositories/ShoppingOrderRepository.php
+++ app/Repositories/ShoppingOrderRepository.php
@@ -19,15 +19,44 @@ class ShoppingOrderRepository
     }
 
     public function getOrderDetail($id)
-{
-    return ShoppingOrder::with([
-        'user',
-        'paymentMethod',
-        'shippingMethod',
-        'address',
-        'orderStatus',
-        'orderLines.productItem.product'
-    ])->find($id);
-}
+    {
+        return ShoppingOrder::with([
+            'user',
+            'paymentMethod',
+            'shippingMethod',
+            'address',
+            'orderStatus',
+            'orderLines.productItem.product'
+        ])->find($id);
+    }
+
+    public function getOrdersByUserId($userId)
+    {
+        return ShoppingOrder::with([
+            'user',
+            'paymentMethod',
+            'shippingMethod',
+            'address',
+            'orderStatus',
+            'orderLines',
+            'orderLines.productItem',
+            'orderLines.productItem.product',
+        ])
+            ->where('user_id', $userId)
+            ->orderByDesc('order_date')
+            ->get();
+    }
+
+    /**
+     * Cập nhật trạng thái đơn hàng
+     */
+    public function updateOrderStatus($orderId, $newStatusId)
+    {
+        $order = ShoppingOrder::findOrFail($orderId);
+        $order->order_status_id = $newStatusId;
+        $order->save();
+        return $order->fresh('orderStatus');
+    }
+
 
 }
